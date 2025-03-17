6a7bc2d971acc6f8dfe8c8a6c048b9578015df7b
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2025-03-17 15:34:53 +0500

    updated

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r", encoding="utf-16") as file:
+    content = file.read()
+
+with open("README.md", "w", encoding="utf-8") as file:
+    file.write(content)
+
 setup(
-    name="youtube_downloader",
-    version="0.0.1",
+    name="youtube_downloader2",
+    version="0.0.3",
     description="A package to download youtube videos and audio from youtube",
     author="Sino Farmonov",
     url="https://github.com/sinofarmonovzfkrvjl/youtube-downloader",
@@ -13,5 +19,7 @@ setup(
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.9",
-    install_requires=["requests"]
+    install_requires=["requests"],
+    long_description=content,
+    long_description_content_type="text/markdown",
 )
