ec746e06e1bd202a579f26d8113d1d9b99c1c495
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2025-03-17 15:00:31 +0500

    updated

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="youtube_downloader",
+    version="0.0.1",
+    description="A package to download youtube videos and audio from youtube",
+    author="Sino Farmonov",
+    
+)
