9a9c398ebd13a2ae85b0500891fea72cd85a5194
sinofarmonovzfkrvjl <farmonovsino4@gmail.com>
2025-03-17 15:05:08 +0500

    updated

--- setup.py
+++ setup.py
@@ -5,5 +5,13 @@ setup(
     version="0.0.1",
     description="A package to download youtube videos and audio from youtube",
     author="Sino Farmonov",
-    
+    url="https://github.com/sinofarmonovzfkrvjl/youtube-downloader",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    install_requires=["requests"]
 )
